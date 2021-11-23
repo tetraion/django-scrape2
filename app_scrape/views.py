cbbfac0191d8fdfbcea16dbcce7da7ad35643b04
Your Name <you@example.com>
2021-11-23 10:45:14 +0900

    test

---
+++ app_scrape/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"]="taichi"
+        return ctxt
